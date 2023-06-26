5015cc0db45904e1c041a7bd10cdbd5e3fc324d1
hdm03 <geg828@nate.com>
2023-06-27 00:33:59 +0900

    Add : tailwind colors 값 추가, tailwind 커스텀 클래스 추가 - color.js 파일 삭제

--- src/contents/color.ts
+++
@@ -1,7 +0,0 @@
-export const Primary = '#F9DA8B';
-export const Secondary = '#212121';
-export const Gray = '#E5E4E3';
-export const LightGary = '#F8F8F8';
-export const DeepGray = '#807F7E';
-export const Error = '#E73B2F';
-export const White = '#ffffff';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Primary: '#F9DA8B',
+        Secondary: '#212121',
+        Gray: '#E5E4E3',
+        LightGary: '#F8F8F8',
+        DeepGray: '#807F7E',
+        Error: '#E73B2F',
+        White: '#ffffff',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        '.headline1': {
+          '@apply font-semibold text-2xl leading-8': '',
+        },
+        '.headline2': {
+          '@apply font-semibold text-xl leading-6': '',
+        },
+        '.headline3': {
+          '@apply font-semibold text-lg leading-6': '',
+        },
+        '.headline4': {
+          '@apply font-semibold text-base leading-5': '',
+        },
+        '.body1': {
+          '@apply font-normal text-base leading-6': '',
+        },
+        '.body2': {
+          '@apply font-normal text-sm leading-5': '',
+        },
+        '.body3': {
+          '@apply font-normal text-xs leading-4': '',
+        },
+        '.normal': {
+          '@apply font-medium text-lg leading-6': '',
+        },
+        '.medium': {
+          '@apply font-medium text-base leading-5': '',
+        },
+        '.small': {
+          '@apply font-medium text-sm leading-4': '',
+        },
+      });
+    },
+  ],
 };
