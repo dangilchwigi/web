1074314383f8bdeb40a3103134533d748bfccabd
hdm03 <geg828@nate.com>
2023-07-03 23:13:10 +0900

    Fix: 비활성화된 아이템 클릭 오류 수정, props 이름 변경 및 기능 추가 - 비활성화된 아이템 클릭 오류 수정 - label을 placeholder로 이름 변경 - defaultValue 기능 추가 - CSS 그림자 추가 - 바깥 클릭 시 아이템 목록 닫히도록 수정 - 스토리북 코드 수정

--- src/components/ui/Dropdown.stories.tsx
+++ src/components/ui/Dropdown.stories.tsx
@@ -1,31 +1,48 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import Dropdown from './Dropdown';
-
 const meta = {
   title: 'Design System/Dropdown',
   component: Dropdown,
   tags: ['autodocs'],
   argTypes: {},
 } satisfies Meta<typeof Dropdown>;
-
 export default meta;
 type Story = StoryObj<typeof meta>;
-
 export const DropdownExample: Story = {
   args: {
-    label: 'Dropdown',
+    placeholder: 'Dropdown',
     items: [
       {
-        label: 'Item 1',
+        label: 'item1',
         value: 'item1',
       },
       {
-        label: 'Item 2',
+        label: 'item2',
         value: 'item2',
       },
       {
-        label: 'Item 3',
+        label: 'item3',
+        value: 'item3',
+        disable: true,
+      },
+    ],
+  },
+};
+export const DropdownDefaultValue: Story = {
+  args: {
+    placeholder: 'Dropdown',
+    defaultValue: 'item1',
+    items: [
+      {
+        label: 'item1',
+        value: 'item1',
+      },
+      {
+        label: 'item2',
+        value: 'item2',
+      },
+      {
+        label: 'item3',
         value: 'item3',
         disable: true,
       },
--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
 type DropdownItem = {
@@ -8,48 +8,84 @@ type DropdownItem = {
 };
 
 interface Props {
-  label: string;
+  placeholder?: string;
   items: DropdownItem[];
+  defaultValue?: string;
   onSelectItemHandler?: (label: string) => void;
 }
 
-const Dropdown = ({ label, items, onSelectItemHandler }: Props) => {
+const Dropdown = ({
+  placeholder,
+  items,
+  defaultValue,
+  onSelectItemHandler,
+}: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [currentItem, setCurrentItem] = useState<DropdownItem | null>(null);
+  const [currentItem, setCurrentItem] = useState('');
 
-  const handleItemClick = (item: DropdownItem) => {
-    setCurrentItem(item);
+  useEffect(() => {
+    if (defaultValue) {
+      setCurrentItem(defaultValue);
+    }
+  }, [defaultValue]);
+
+  useEffect(() => {
+    const closeDropdown = (event: MouseEvent) => {
+      if (
+        event.target instanceof HTMLElement &&
+        !event.target.closest('.dropdown-wrapper')
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('click', closeDropdown);
+    } else {
+      document.removeEventListener('click', closeDropdown);
+    }
+
+    return () => document.removeEventListener('click', closeDropdown);
+  }, [isOpen]);
+
+  const handleItemClick = (value: string) => {
+    setCurrentItem(value);
     setIsOpen(false);
     if (onSelectItemHandler) {
-      onSelectItemHandler(item.value);
+      onSelectItemHandler(value);
     }
   };
 
   return (
-    <div className='relative'>
+    <div className='dropdown-wrapper relative'>
       <button
-        className='border-[1px] border-Primary px-4 py-3  flex justify-between w-[300px]'
-        onClick={() => setIsOpen(!isOpen)}
+        className='border-[1px] border-Primary px-4 py-3 flex justify-between w-[300px] rounded'
+        onClick={(event) => {
+          event.stopPropagation();
+          setIsOpen(!isOpen);
+        }}
       >
-        <span className='body1'>{currentItem?.label ?? label}</span>
+        <span className='body1'>
+          {currentItem !== '' ? currentItem : placeholder}
+        </span>
         <MdKeyboardArrowDown
           size={24}
           className={`text-Primary ${isOpen && 'rotate-180'}`}
         />
       </button>
       {isOpen && (
-        <div className='w-fit max-h-[240px] overflow-scroll'>
+        <div className='my-3 absolute w-fit max-h-[240px] overflow-y-scroll z-10 rounded shadow-[0_4px_8px_1px_rgba(0,0,0,0.1)]'>
           {items.map((item) => (
             <button
               key={item.value}
               className={`w-[300px] h-[48px] px-4 py-3 body1 flex justify-start ${
-                currentItem?.value === item.value
+                currentItem === item.value
                   ? 'text-Primary'
                   : item.disable
-                  ? 'text-Gray'
+                  ? 'text-Gray pointer-events-none'
                   : 'text-Secondary'
               } `}
-              onClick={() => handleItemClick(item)}
+              onClick={() => handleItemClick(item.value)}
             >
               {item.label}
             </button>
